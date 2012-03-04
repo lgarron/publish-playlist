f2d35cd7a5fe4c92710383b430f2e916967c9827
Lucas Garron <lucasg@gmx.de>
2012-03-03 17:29:02 -0800

    Script updates from the last few months.

--- publish-playlist.sh
+++ publish-playlist.sh
@@ -13,11 +13,11 @@
 
 #Configure:
 
-# Your Dropbox Public Folder. WITHOUT trailing slash.
-PUBLIC_FOLDER="/Users/lgarron/Dropbox/Public"
+# Your Dropbox Public Folder. WITH trailing slash.
+PUBLIC_FOLDER="/Users/lgarron/Dropbox/Public/"
 
 # The folder where you want to store your playlists (must already exist, and be in the public folder). WITHOUT trailing slash.
-MUSIC_FOLDER_NAME="Music"
+MUSIC_FOLDER="/Users/lgarron/Dropbox/Public/Music"
 
 # Your Dropbox ID. Can be found be looking at a public URL for any file in your shared folder.
 DROPBOX_ID="13568303"
@@ -27,32 +27,24 @@ PLAYLIST_PREFIX="Playlist-"
 
 ######
 
-echo ""
-
-MUSIC_FOLDER="$PUBLIC_FOLDER/$MUSIC_FOLDER_NAME"
-URL_PATH="http://dl.dropbox.com/u/$DROPBOX_ID/$MUSIC_FOLDER_NAME"
+RELATIVE_FOLDER_NAME=`echo "$MUSIC_FOLDER" | sed "s@$PUBLIC_FOLDER@@"`
+URL_PATH="http://dl.getdropbox.com/u/$DROPBOX_ID/$RELATIVE_FOLDER_NAME"
 
 M3U_ORIGINAL_FILE="$1"
 M3U_ORIGINAL_FULL_NAME=`basename "$M3U_ORIGINAL_FILE"`
 
 M3U_NAME="${M3U_ORIGINAL_FULL_NAME%.*}"
-M3U_FILE_NAME="$PLAYLIST_PREFIX$M3U_NAME.m3u"
-M3U_FILE="$MUSIC_FOLDER/$M3U_FILE_NAME"
+M3U_FILE="$MUSIC_FOLDER/$PLAYLIST_PREFIX$M3U_NAME.m3u"
 PLAYLIST_FOLDER_NAME="$PLAYLIST_PREFIX$M3U_NAME"
 PLAYLIST_FOLDER="$MUSIC_FOLDER/$PLAYLIST_PREFIX$M3U_NAME"
 
-M3U_FILE_URL="$URL_PATH/$M3U_FILE_NAME"
+M3U_FILE_URL="$URL_PATH/$M3U_FILE"
 M3U_FILE_URL=`echo "$M3U_FILE_URL" | sed 's/\ /%20/g'`
 
-if [ ! -d "$MUSIC_FOLDER" ]; then
-  echo "Creating folder: $MUSIC_FOLDER"
-  mkdir "$MUSIC_FOLDER"
-fi
-
-mkdir "$PLAYLIST_FOLDER"
+mkdir "$MUSIC_FOLDER/$PLAYLIST_FOLDER_NAME"
 
 touch "$M3U_FILE"
-echo -n "" > "$M3U_FILE"
+#echo -n "" > "$M3U_FILE"
 
 cat "$M3U_ORIGINAL_FILE" | tr '\r' '\n' |
 while read SONG_FILE
@@ -62,7 +54,6 @@ then
     echo "$SONG_FILE" >> "$M3U_FILE"
 else
     SONG_FILE_NAME=`basename "$SONG_FILE"`
-    echo "[Song] $SONG_FILE_NAME..."
     ln -s "$SONG_FILE" "$PLAYLIST_FOLDER/$SONG_FILE_NAME"
     FILE_URL="$URL_PATH/$PLAYLIST_FOLDER_NAME/$SONG_FILE_NAME"
     FILE_URL=`echo "$FILE_URL" | sed 's/\ /%20/g'`
@@ -70,8 +61,8 @@ else
 fi
 done
 
+cp "$M3U_FILE" "$M3U_FILE.txt"
+
+echo "$M3U_FILE_URL"
 echo "$M3U_FILE_URL" | pbcopy
-echo ""
-echo "$M3U_FILE_URL (should be on your clipboard now)"
-echo ""
-# qs "$M3U_FILE"
+qs "$M3U_FILE"
